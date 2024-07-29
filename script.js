8083695b5c321154e4173d91bcc5bdf5784caec4
Dave Ring <davealring@gmail.com>
2024-07-29 10:21:43 -0400

    combined functions into single function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function isEven(num) {
     return num % 2 == 0;
 };
 
-function multBythree(num) {
+function multByThree(num) {
     return num * 3;
 };
 
@@ -13,11 +13,21 @@ function addTotal(sum, num) {
 let arr = [1,2,3,4,5,6,7,8,9,10];
 console.log(arr);
 
-let filteredArr = arr.filter(isEven);
-console.log(filteredArr);
+// let filteredArr = arr.filter(isEven);
+// console.log(filteredArr);
 
-let multiArr = filteredArr.map(multBythree);
-console.log(multiArr);
+// let multiArr = filteredArr.map(multByThree);
+// console.log(multiArr);
 
-let reducedArr = multiArr.reduce(addTotal);
-console.log(reducedArr);
+// let reducedArr = multiArr.reduce(addTotal);
+// console.log(reducedArr);
+
+function sumOfTripledEvens(arr) {
+    return arr
+        .filter(isEven)
+        .map(multByThree)
+        .reduce(addTotal);
+};
+
+let finalArr = sumOfTripledEvens(arr);
+console.log(finalArr);
